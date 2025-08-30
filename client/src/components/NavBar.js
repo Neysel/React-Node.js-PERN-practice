6cc4122af903864121150e12e1e76e9b66eb4729
Neysel <neyseltheblack@gmail.com>
2025-08-31 03:02:38 +0400

    updated navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,24 +3,35 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useContext } from 'react';
- import {Context} from "../index" 
+import {Context} from "../index" 
+import { SHOP_ROUTE } from '../utils/const';
+import { NavLink } from 'react-router-dom';
+import {Button} from "react-bootstrap"
+import {observer} from "mobx-react-lite"
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return ( 
     <div>
-        <NavBar bg="dark" data-bs-theme="dark">
-        <Container>
-          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </NavBar>
+        <nav style={{color:'white', padding: '15px'}} class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <Container>
+            <NavLink style={{color:'#bbdeff', fontSize:"20px", textDecoration: "none"}} to={SHOP_ROUTE}>BuySomething</NavLink>
+       
+            {user.isAuth ? 
+                  <Nav style={{color:'white'}} className="ml-auto">
+                    <Button>Admin panel</Button>
+                    <Button onClick={() => user.setIsAuth(false)} className='ms-4'>Log out</Button>
+                 </Nav>
+                :
+                <Nav style={{color:'white'}} className="ml-auto">
+                    <Button  onClick={() => {user.setIsAuth(true); console.log(user.isAuth)}}>Authorization</Button>
+                </Nav>
+            }
+              </Container>
+        </nav> 
+  
     </div>
      );
-}
+})
  
 export default NavBar;
--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Card, Container, Form } from 'react-bootstrap';
 
+// http://localhost:3000/login
 
 const Auth = () => {
     return ( 
         <div>
-            AUTH
+            <Container 
+              className='d-flex justify-content-center align-items-center'
+                style={{height: window.innerHeight - 54}}
+            >
+                <Card style={{width: 600}} className='p-5'>
+                    <h2 className='m-auto'>Authorization</h2>
+                    <Form>
+
+                    </Form>
+                </Card>
+            </Container>
         </div>
      );
 }
