1d93723e84ef7bdb4f75d3dec757372dafd5c655
Neysel <neyseltheblack@gmail.com>
2025-08-31 22:01:56 +0400

    main frontend part is over

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import {Context} from "../../index" 
+import { Dropdown, Row, Col } from 'react-bootstrap';
 
 const CreateDevice = ({show, onHide}) => {
+    const {device} = useContext(Context)
+    const [info, setInfo] = useState([])
+
+    const addInfo = () => {
+        setInfo([...info, {title: '', description: '',  number: Date.now() }])
+    }
+
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number ))
+    }
+
     return ( 
         <div>
              <Modal
@@ -21,9 +34,58 @@ const CreateDevice = ({show, onHide}) => {
       </Modal.Header>
       <Modal.Body>
             <Form>
-                <Form.Control
-                    placeholder={`add device name`}
-                />
+                {/* <Form.Control
+                    // placeholder={`add device name`}
+                   
+                /> */}
+                    <Dropdown>
+                     <Dropdown.Toggle>Choose type</Dropdown.Toggle>
+                     <Dropdown.Menu>
+                        {device.types.map(type => <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>)}
+                     </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown className='mt-2'>
+                     <Dropdown.Toggle>Choose brand</Dropdown.Toggle>
+                     <Dropdown.Menu>
+                        {device.types.map(brand => <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>)}
+                     </Dropdown.Menu>
+                    </Dropdown>
+                    <Form.Control 
+                        className='mt-3'
+                        placeholder='add device name'>
+                    </Form.Control>
+                     <Form.Control 
+                        className='mt-3'
+                        placeholder='add price'
+                        type='number'
+                        >
+                    </Form.Control>
+                     <Form.Control 
+                        className='mt-3'
+                        // placeholder='add device name'
+                        type='file'
+                        >
+                    </Form.Control>
+                    <hr/>
+                    <Button onClick={addInfo}
+                    >Add new property</Button>
+                    { info.map(i => 
+                        <Row key={i.nummber} className='mt-4'>
+                            <Col md={4}  className='mt-2'>
+                                <Form.Control placeholder='Property name' />
+                            </Col>
+                            <Col md={4}  className='mt-2'>
+                                <Form.Control placeholder='Property description' />
+                            </Col>
+                             <Col md={4}  className='mt-2'>
+                                <Button 
+                                onClick={() => removeInfo(i.number)}
+                                variant={"outline-danger"}>
+                                    Delete
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
             </Form>
       </Modal.Body>
       <Modal.Footer>
