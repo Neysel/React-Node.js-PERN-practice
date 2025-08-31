4f5c1979116f07178d17b8e4e53d591d9dab6cc1
Neysel <neyseltheblack@gmail.com>
2025-08-31 05:24:13 +0400

    login and registration window done

--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { Card, Container, Form } from 'react-bootstrap';
+import { Card, Container, Form, Row } from 'react-bootstrap';
+import {Button} from "react-bootstrap"
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/const';
+import { NavLink, useLocation } from 'react-router-dom';
 
 // http://localhost:3000/login
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location)
+
     return ( 
         <div>
             <Container 
               className='d-flex justify-content-center align-items-center'
                 style={{height: window.innerHeight - 54}}
             >
-                <Card style={{width: 600}} className='p-5'>
-                    <h2 className='m-auto'>Authorization</h2>
-                    <Form>
-
+                <Card style={{width: 600}} className='p-5' >
+                    <h2 className='m-auto'>{isLogin ? `Authorization`: `Registration`}</h2>
+                    <Form className='d-flex flex-column'>
+                        <Form.Control 
+                            className='mt-3'
+                            placeholder='enter email'
+                        />
+                        <Form.Control 
+                            className='mt-3'
+                            placeholder='enter password'
+                        />
+                        <Row>
+                            <div className='mt-3' style={{textAlign: "center"}}>
+                                {isLogin ? 
+                                <span>Don't have account?`<NavLink to={REGISTRATION_ROUTE}>Register</NavLink></span>
+                                : <span>Already have account?<NavLink to={LOGIN_ROUTE}>Log in</NavLink> </span>}
+                            </div>
+                        </Row>
+                        <Button className='mt-3'>
+                            Enter
+                        </Button>
                     </Form>
                 </Card>
             </Container>
