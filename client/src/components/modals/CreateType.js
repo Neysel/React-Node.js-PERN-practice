6c752c513b7f0fa8e00e3c7a9e4b0b55c691ad57
Neysel <neyseltheblack@gmail.com>
2025-09-01 02:58:38 +0400

    creation types added

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import { createType } from '../../http/deviceAPI';
 
 const CreateType = ({show, onHide}) => {
-
-  // const addType
+  const [value, setValue] = useState('')
+  const addType = () => {
+    createType({name: value}).then(data => setValue(''))
+    onHide()
+  }
 
     return ( <div>
            <Modal
@@ -24,12 +29,14 @@ const CreateType = ({show, onHide}) => {
       <Modal.Body>
             <Form>
                 <Form.Control
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
                     placeholder={`add type name`}
                 />
             </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Add new type</Button>
+        <Button onClick={addType} >Add new type</Button>
         <Button onClick={onHide} variant='outline-danger'>Close</Button>
       </Modal.Footer>
     </Modal>
